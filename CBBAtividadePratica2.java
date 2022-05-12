0ce07e68a4244db362954ea18b49b664a0b4bdf3
raszz <50380670+raszz@users.noreply.github.com>
2022-05-11 22:19:45 -0300

    adicionando Questao2CBB

---
+++ CBBAtividadePratica2.java
@@ -0,0 +1,11 @@
+public class CBBAtividadePratica2 {
+    public static void main(String[] args) throws Exception {
+    int a = 3;
+    int b = 5;
+    int c = 9;
+    int soma = a+b+c;
+    float media = soma/3f;
+    System.out.println("a soma eh = " + soma);
+    System.out.println("a media eh = " + media);
+    }
+}
