c5cfbbe744f2fa62c8fe670762225cc3f9ed47f6
raszz <50380670+raszz@users.noreply.github.com>
2022-05-13 16:03:54 -0300

    Corrigindo e atualizando BMExercio03.java

--- BMExercio03.java
+++ BMExercio03.java
@@ -1,8 +1,6 @@
-import java.util.Scanner;
 public class BMExercio03 {
     
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
         double item1 = 2.95;
         double item2 = 3.50; //A
 
@@ -10,24 +8,21 @@ public class BMExercio03 {
         System.out.println("O item 2 custa " + item2); // B
 
         double ValorTotal = item1 + item2;
-        System.out.println("" + ValorTotal); //C
+        System.out.println("O total dos itens eh " + ValorTotal); //C
 
         final double taxa = 8.25 /100; //D
         
-        double taxaCalculada = ValorTotal + (taxa * ValorTotal); 
-        System.out.println("" + taxaCalculada);
+        double taxaCalculada = taxa * ValorTotal; 
+        System.out.println("A taxa calculada eh " + taxaCalculada);
 
         double novoCusto = (item1 + taxaCalculada) + (item2 + taxaCalculada); //E
-        System.out.println("" + novoCusto);
+        System.out.println("O novo custo eh " + novoCusto);
 
-        System.out.println("\n o novo total é "+novoCusto); //f
+    
 
-         boolean muitoCaro = (novoCusto >10);
+        boolean muitoCaro = (novoCusto >10); //F
 
-         System.out.println(""+muitoCaro);
-
-       
-        
+        System.out.println(""+muitoCaro);   
     }
 
 }
